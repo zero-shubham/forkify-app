80504395f21ef5dd2a4d58c62901ffb1568d36ca
zero <codeweed0@gmail.com>
2018-09-28 12:04:03 +0530

    idk

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 // Global app controller
 
 import Search from './models/Search';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 import * as searchView from './views/searchView';
+
 /* GLobal state of the app
 *- Search object
 *- Current recipe object
@@ -22,11 +23,13 @@ const controlSearch = async () => {
         //3) Prepare UI for results
         searchView.clearInput();
         searchView.clearPrevResult();
-        
+        renderLoader(elements.searchRes);
+
         //4) Search for recipes
         await state.search.getResults();
 
         //5) render results to UI
+        clearLoader();
         searchView.renderResults(state.search.recipe);
     }
 }
